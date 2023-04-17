3831fadb160cf351684e4c326ef171776b2fedf3
Darshi Shah <darshi24.shah@gmail.com>
2023-04-17 00:30:44 -0400

    Adding options for session

--- app.js
+++ app.js
@@ -8,19 +8,23 @@ import SessionController from "./controllers/session-controller.js";
 import ReviewsController from "./controllers/reviews-controller.js";
 import FollowsController from "./controllers/follows-controller.js";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+const CONNECTION_STRING = "mongodb+srv://darshi24:darshi24@cluster0.wwke7rm.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.set('trust proxy',1);
+// app.set('trust proxy',1);
 app.use(express.json());
 app.use(cors({
     credentials : true,
-    origin : 'https://preeminent-cranachan-3c8ddf.netlify.app'
+    // origin : 'https://preeminent-cranachan-3c8ddf.netlify.app'
+    origin : 'http://localhost:3000'
 }));
 app.use(session({
     secret : 'process.env.SECRET',
-    cookie : {secure : true}
+    resave : false,
+    saveUninitialized : true,
+    cookie : {secure : false}
 
 }))
 
